b101ffc853b72ce4d8bfa8a2e108779442c61d65
Chelsea Hitzler <chelsea@lamproslabs.com>
2021-01-14 17:20:42 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -4,14 +4,42 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function header() {
   return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h1>I'm testing bootstrap</h1>
+      <div className="image-header">
+        <div className="blur py-4">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 ">
+                <div className="d-inline-block align-middle">
+                  <h1 className="white">CHELSEA'S</h1>
+                  <h1 className="favorite-heading white">FAVORITE THINGS</h1>
+                  <p className="white" >Learn a few fun facts about things I love. Hover over the images to learn more.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
   );
 }
 
+function food(){
+  return(
+    <div className="food">
+      <h1 className="text-center"></h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="column-image"></div>
+          </div>
+          <div className="col-md-4">
+            <div className="column-image"></div>
+          </div>
+          <div className="col-md-4">
+            <div className="column-image"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default header;
