a797f4f65974bfd302488f91d4fd4469595c8c13
Chelsea Hitzler <chelsea@lamproslabs.com>
2021-01-28 16:36:00 -0500

    added a description

--- src/header.js
+++ src/header.js
@@ -12,7 +12,7 @@ function Header() {
                 <div className="d-inline-block align-middle">
                   <h1 className="white">CHELSEA'S</h1>
                   <h1 className="favorite-heading white">FAVORITE THINGS</h1>
-                  <p className="white" >Learn a few fun facts about things I love. Hover over the images to learn more.</p>
+                  <p className="white" >Learn a little about me by looking at some of my favorite things! I love trying new food, meeting animals, and playing video games. I have plenty other interests but learn a few fun facts about things I love by hovering over the images titles.</p>
                 </div>
               </div>
             </div>
